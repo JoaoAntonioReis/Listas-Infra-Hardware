39a242859c3489a24e61b3a30e21c0c8464807dd
João Antonio de Lima Reis <100496268+JoaoAntonioReis@users.noreply.github.com>
2025-05-26 09:36:31 -0300

    Update Questão1.asm

--- Lista 1- MIPS/Questão1.asm
+++ Lista 1- MIPS/Questão1.asm
@@ -9,17 +9,19 @@
 	
 	li $v0, 5 #Le valor do segundo inteiro
 	syscall
-	
-	move $t1, $v0 #move o valor do primero inteiro para o registrador t0
-	
-	
+
+	move $t1, $v0 #move o valor do segundo inteiro para o registrador t1
+
+
 	blt $t0, $t1, menor
 	bgt $t0, $t1, maior
 	add $t2, $t0, $t1
 	syscall
 	menor: 
-		move $t2, $t0
+		
+		move $t2, $t1
 		syscall
 	maior: 
-		move $t2, $t1
+		
+		move $t2, $t0
 		syscall
